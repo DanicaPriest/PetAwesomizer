4a5e81dd465582031b5d572f00e982098bd7277c
Danica <Danicamaypriest@hotmail.co.uk>
2018-04-06 16:32:15 +0800

    update still not working

--- src/main/java/controller/PetAwesomizerController.java
+++ src/main/java/controller/PetAwesomizerController.java
@@ -1,4 +1,25 @@
-package controller;
+package mybatis.controller;
 
+
+
+import model.PetRoot;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import service.PetAwesomizerService;
+
+
+@RestController
+@RequestMapping("/petawesomizer")
 public class PetAwesomizerController {
+
+    @Autowired
+    PetAwesomizerService petAwesomizerService;
+
+    @RequestMapping("/")
+    public PetRoot getPet() {
+        return petAwesomizerService.searchPets();
+
+    }
+
 }
--- src/main/java/mapper/PetAwesomizerMapper.java
+++ src/main/java/mapper/PetAwesomizerMapper.java
@@ -1,4 +1,7 @@
-package mapper;
+package mybatis.mapper;
 
+import org.apache.ibatis.annotations.Mapper;
+
+@Mapper
 public class PetAwesomizerMapper {
 }
