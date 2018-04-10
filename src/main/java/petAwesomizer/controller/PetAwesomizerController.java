0e3e922ab3474af5cb7e6d46c38af44de9946eba
Danica <Danicamaypriest@hotmail.co.uk>
2018-04-10 19:16:51 +0800

    got the JMV workinggit status

--- src/main/java/petAwesomizer/controller/PetAwesomizerController.java
+++ src/main/java/petAwesomizer/controller/PetAwesomizerController.java
@@ -2,6 +2,7 @@ package petAwesomizer.controller;
 
 
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.ModelAndView;
 import petAwesomizer.model.PetRoot;
 import org.springframework.beans.factory.annotation.Autowired;
 import petAwesomizer.model.PetSimplified;
@@ -18,30 +19,47 @@ public class PetAwesomizerController {
     PetAwesomizerService petAwesomizerService;
 
     @RequestMapping("/search")
-    public PetRoot searchPet(@RequestParam (value="location", defaultValue = "virginia")String location,
-                             @RequestParam (value="animal", required= false, defaultValue = "") String animal) {
+    public PetRoot searchPet(@RequestParam(value = "location", defaultValue = "virginia") String location,
+                             @RequestParam(value = "animal", required = false, defaultValue = "") String animal) {
 
-        return petAwesomizerService.searchPets(location, animal); }
+        return petAwesomizerService.searchPets(location, animal);
+    }
 
     @RequestMapping("/")
-    public ArrayList<PetSimplified> mapPet(@RequestParam (value="location", defaultValue = "virginia")String location,
-                                           @RequestParam (value="animal", required= false, defaultValue = "") String animal) {
+    public ArrayList<PetSimplified> mapPet(@RequestParam(value = "location", defaultValue = "virginia") String location,
+                                           @RequestParam(value = "animal", required = false, defaultValue = "") String animal) {
 
-        return petAwesomizerService.mapPets(location, animal); }
+        return petAwesomizerService.mapPets(location, animal);
+    }
 
-    @RequestMapping("/random")
-    public PetSimplified randomPet(
+    @RequestMapping(value= "/random", method = RequestMethod.GET)
+    public ModelAndView getRandom(
             @RequestParam (value="animal", required= false, defaultValue = "") String animal,
-            @RequestParam (value="breed", required= false, defaultValue = "") String breed) {
+             @RequestParam (value="breed", required= false, defaultValue = "") String breed
+    ) {
+        ModelAndView modelAndView = new ModelAndView();
+        PetSimplified pet = petAwesomizerService.getRandomPet(animal, breed);
 
-        return petAwesomizerService.getRandomPet(animal, breed); }
+        modelAndView.addObject("petName", pet.getName());
+        modelAndView.addObject("petPhoto", pet.getPhoto());
+        modelAndView.addObject("animal", "Animal: " + pet.getAnimal());
+        modelAndView.addObject("sex", "Sex: " + pet.getSex());
+        modelAndView.addObject("age", "Age: " + pet.getAge());
+        modelAndView.addObject("location", "Location: " + pet.getLocation());
+        modelAndView.addObject("email", "Contact email: " + pet.getEmail());
+        modelAndView.addObject("petFact", pet.getDescription());
+        modelAndView.setViewName("random");
+
+        return modelAndView;
+    }
 
     @RequestMapping(method = RequestMethod.PUT, value = "/load")
-    public ArrayList<PetSimplified> loadPets(@RequestParam (value="location", defaultValue = "virginia")String location,
-                                             @RequestParam (value="animal", required= false, defaultValue = "") String animal) {
-        ArrayList<PetSimplified> pets = petAwesomizerService.mapPets(location,animal);
+    public ArrayList<PetSimplified> loadPets(@RequestParam(value = "location", defaultValue = "virginia") String location,
+                                             @RequestParam(value = "animal", required = false, defaultValue = "") String animal) {
+        ArrayList<PetSimplified> pets = petAwesomizerService.mapPets(location, animal);
         petAwesomizerService.insertPets(pets);
 
-        return pets; }
+        return pets;
+    }
 
 }
